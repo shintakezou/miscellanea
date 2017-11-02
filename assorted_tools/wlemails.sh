c4442648ebab724e57e41573b31dea0c8949edf8
shintakezou <shintakezou@gmail.com>
2017-11-02 21:11:32 +0100

    save also the wgot page

--- assorted_tools/wlemails.sh
+++ assorted_tools/wlemails.sh
@@ -16,7 +16,7 @@ pushd $eid
 
 # change user agent as you like
 wget -U "Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.99 Safari/537.36" \
-     "https://wikileaks.org/${theme}/emails/emailid/$eid" -O- |xmllint --html --xpath $'//div[@id="email_raw"]/pre/text()' - |sed -e 's/&lt;/</g; s/&gt;/>/g; s/&amp;/&/g' >${eid}.eml
+     "https://wikileaks.org/${theme}/emails/emailid/$eid" -O- |tee ${eid}-page.html - | xmllint --html --xpath $'//div[@id="email_raw"]/pre/text()' - |sed -e 's/&lt;/</g; s/&gt;/>/g; s/&amp;/&/g' >${eid}.eml
 
 exmime=$(which extractmime.py)
 if [[ $? -eq 0 ]]; then
